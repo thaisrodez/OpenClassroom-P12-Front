4fa63c16c0595d5d78e833a5f0f006ae9593ccea
thais de rodez <thaisderodez@gmail.com>
2023-04-06 17:54:48 +0200

    feat(router): implement router

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/:id" element={<App />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
