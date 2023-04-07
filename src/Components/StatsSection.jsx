80ba1a37052965a7652be204baa357bb2d1e44bf
thais de rodez <thaisderodez@gmail.com>
2023-04-07 09:39:41 +0200

    feat(stats): get stats data

--- src/Components/StatsSection.jsx
+++ src/Components/StatsSection.jsx
@@ -2,9 +2,11 @@ import caloriesIcon from '../assets/calories-icon.png';
 import carbsIcon from '../assets/carbs-icon.png';
 import fatIcon from '../assets/fat-icon.png';
 import proteinIcon from '../assets/protein-icon.png';
-import { user } from '../mockedDatas/mockedData.js';
 import styled from 'styled-components';
 import Stat from './Stat';
+import { useApi } from '../service/api.js';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 
 const StatsContainer = styled.section`
   display: flex;
@@ -13,24 +15,41 @@ const StatsContainer = styled.section`
 `;
 
 function StatsSection() {
+  const params = useParams();
+  const [keyData, setkeyData] = useState();
+
+  const { data, error, isLoading } = useApi({
+    method: 'GET',
+    url: `/${params.id}`,
+  });
+
+  useEffect(() => {
+    if (data) {
+      setkeyData(data.data.keyData);
+    }
+  }, [data]);
   return (
     <StatsContainer>
-      <Stat
-        icon={caloriesIcon}
-        count={user.keyData.calorieCount}
-        type="calories"
-      />
-      <Stat
-        icon={proteinIcon}
-        count={user.keyData.proteinCount}
-        type="protein"
-      />
-      <Stat
-        icon={carbsIcon}
-        count={user.keyData.carbohydrateCount}
-        type="carbohydrate"
-      />
-      <Stat icon={fatIcon} count={user.keyData.lipidCount} type="lipid" />
+      {!isLoading && keyData && (
+        <>
+          <Stat
+            icon={caloriesIcon}
+            count={keyData.calorieCount}
+            type="calories"
+          />
+          <Stat
+            icon={proteinIcon}
+            count={keyData.proteinCount}
+            type="protein"
+          />
+          <Stat
+            icon={carbsIcon}
+            count={keyData.carbohydrateCount}
+            type="carbohydrate"
+          />
+          <Stat icon={fatIcon} count={keyData.lipidCount} type="lipid" />
+        </>
+      )}
     </StatsContainer>
   );
 }
